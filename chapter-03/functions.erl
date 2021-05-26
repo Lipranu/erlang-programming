4b44c4091bb5341200cc4aeb9c544e4fa3430a6d
Igor Belousov <lipranu@gmail.com>
2021-05-26 17:58:32 +0700

    Complete exercise 3-3

--- chapter-03/functions.erl
+++ chapter-03/functions.erl
@@ -1,5 +1,7 @@
 -module(functions).
--export([sum/1, sum/2, sumt/1, sumt/2, create/1, create_reverse/1]).
+-export([sum/1, sum/2, sumt/1, sumt/2]).
+-export([create/1, create_reverse/1]).
+-export([print_all/1, print_even/1, const_true/1, even/1]).
 
 % Exercise 3-1
 
@@ -38,3 +40,26 @@ create_reverse(N) when N =< 0, is_integer(N) ->
     [];
 create_reverse(N) when N > 0, is_integer(N) ->
     [N | create_reverse(N - 1)].
+
+% Exercise 3-2
+
+print_all(N) when is_integer(N), N > 0 ->
+    print_all_inner(1, N, const_true).
+
+print_even(N) when is_integer(N), N > 0 ->
+    print_all_inner(1, N, even).
+
+const_true(_) ->
+    true.
+
+even(N) when is_integer(N) ->
+    N rem 2 == 0.
+
+print_all_inner(N, M, _) when N > M ->
+    ok;
+print_all_inner(N, M, F) ->
+    case apply(functions, F, [N]) of
+      true -> io:format("Number:~p~n",[N]);
+      false -> ok
+    end,
+    print_all_inner(N + 1, M, F).
